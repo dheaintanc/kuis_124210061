f3bb4888e1d7cc3e9660f24e161fd1c8a773515a
dheaintanc <dheaintancc@gmail.com>
2023-10-03 21:57:17 +0700

    first commit

---
+++ lib/book_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:kuismobile/book_list.dart';
+class BookList extends StatelessWidget {
+  const BookList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
